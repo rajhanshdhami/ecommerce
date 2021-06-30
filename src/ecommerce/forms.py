1312543baab1cdea1ba36b26a84bff5908eb1d70
rajhanshdhami <dhamirajhansh.007@gmail.com>
2021-06-30 16:48:27 +0530

    user login and register added

--- src/ecommerce/forms.py
+++ src/ecommerce/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-
+from django.contrib.auth import get_user_model
+User = get_user_model()
 class ContactForm(forms.Form):
     full_name = forms.CharField(
         widget = forms.TextInput(
@@ -39,4 +40,91 @@ class ContactForm(forms.Form):
         return email
 
 class LoginForm(forms.Form):
-    username=      
+    user_name = forms.CharField(
+        widget = forms.TextInput(
+            attrs={
+                "class" : "form-control",
+                "id" : "form-full-name",
+                "placeholder" : "Name",
+                "name" : "user_name"
+            }
+        )
+    )
+    password = forms.CharField(
+        widget = forms.PasswordInput(
+            attrs = {
+                "class" : "form-control",
+                "id" : "form_pass_id",
+                "placeholder" : "Password" ,
+                "name" : "password"
+            }
+        )
+    )
+
+class RegisterForm(forms.Form):
+    username = forms.CharField(
+        widget = forms.TextInput(
+            attrs = {
+                "class" : "form-control",
+                "id" : "form_full_name",
+                "placeholder" : "Name",
+                "name" : "user_name"
+            }
+        )
+    )      
+    email = forms.EmailField(
+        widget = forms.EmailInput(
+            attrs= {
+                "class" : "form-control",
+                "id" : "form_email",
+                "placeholder" : "email",
+                "name" : "email"
+            }
+        )
+    )
+    password = forms.CharField(
+        widget = forms.PasswordInput(
+            attrs = {
+                "class" : "form-control",
+                "id" : "form_pass_id",
+                "placeholder" : "Password" ,
+                "name" : "password"
+            }
+        )
+    )
+    confirm_password = forms.CharField(
+        widget = forms.PasswordInput(
+            attrs = {
+                "class" : "form-control",
+                "id" : "form_pass_id",
+                "placeholder" : "Password" ,
+                "name" : "confirm_password"
+            }
+        )
+    )
+    
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        qs = User.objects.filter(username=username)
+        if qs.exists():
+            raise forms.ValidationError("Username is taken")
+        return username
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        qs = User.objects.filter(email=email)
+        if qs.existes():
+            raise forms.ValidationError("email is taken")
+        return email    
+
+    def clean(self):
+        data = self.cleaned_data
+        print(data)
+        password = self.cleaned_data.get("password")
+        confirm_password = self.cleaned_data.get("confirm_password")
+        if password != confirm_password:
+            raise forms.ValidationError("Password must match")
+        else:
+            print("password matches")
+            
+
+        return data          
--- src/ecommerce/views.py
+++ src/ecommerce/views.py
@@ -1,7 +1,82 @@
+from django.contrib.auth import authenticate, login ,get_user_model
 from django.http import HttpResponse
-from django.shortcuts import render
-from .forms import ContactForm
+from django.shortcuts import render, redirect
+from .forms import ContactForm, LoginForm, RegisterForm
 
+def home_page(request):
+    context = {
+        "title" : "Hello world",
+        "content" : "Welcome to the home page"
+      }
+    if request.user.is_authenticated():
+        context["premium_content"]= "you are inside the bubble"  
+    return render(request, "home_page.html", context)   
+
+def about_page(request):
+    context = {
+        "title" : "about page",
+        "content" : "welcome to about page"
+    }    
+    return render(request, "home_page.html", context) 
+
+def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
+    context = {
+        "title" : "contact page",
+        "content" : "welcome to contact page",
+        "form"    : contact_form
+        }    
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    #commented because we are passing request.POST in contactform instace
+    """if request.method == "POST":
+        print(request.POST)
+        print(request.POST.get("full_name"))
+        print(request.POST.get("email"))
+        print(request.POST.get("content"))"""
+    return render(request, "contact/view.html", context)      
+
+def login_page(request):
+    context = {}
+    form = LoginForm(request.POST or None)
+    #predefined django method for authentication
+    context["form"] = form
+    print("User logged in: ",request.user.is_authenticated)
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("user_name")
+        password = form.cleaned_data.get("password")
+        user = authenticate(username=username, password=password)
+        print("our user",user)
+        if user is not None:
+            #print(request.user.is_authenticated())
+            login(request, user)
+            # A backend authenticated the credentials
+            #context['form'] = LoginForm()
+            return redirect("/login")
+        else:
+            # No backend authenticated the credentials  
+            print("Error")      
+    return render(request,"auth/login.html", context)
+
+User = get_user_model()  #predefined method
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+    context = {
+        "form" : form
+    }
+    print("valid check")
+    print(form.is_valid())
+    if form.is_valid():
+        print("valid")
+        print(form.cleaned_data)
+        context['form'] = RegisterForm()
+        username = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = User.objects.create_user(username, email, password)
+        print(user)
+    return render(request, "auth/register.html", context)    
 
 def home_page_old(request):
     html_="""<!doctype html>
@@ -33,40 +108,3 @@ def home_page_old(request):
   </body>
 </html>"""
     return HttpResponse(html_)
-
-def home_page(request):
-    context = {
-        "title" : "Hello world",
-        "content" : "Welcome to the home page"
-    }
-    return render(request, "home_page.html", context)   
-
-def about_page(request):
-    context = {
-        "title" : "about page",
-        "content" : "welcome to about page"
-    }    
-    return render(request, "home_page.html", context) 
-
-def contact_page(request):
-    contact_form = ContactForm(request.POST or None)
-    context = {
-        "title" : "contact page",
-        "content" : "welcome to contact page",
-        "form"    : contact_form
-        }    
-    if contact_form.is_valid():
-        print(contact_form.cleaned_data)
-    #commented because we are passing request.POST in contactform instace
-    """if request.method == "POST":
-        print(request.POST)
-        print(request.POST.get("full_name"))
-        print(request.POST.get("email"))
-        print(request.POST.get("content"))"""
-    return render(request, "contact/view.html", context)      
-
-def login_page(request):
-    return render(request,"auth/login.html", {})
-
-def register_page(request):
-    return render(request, "auth/register.html", {})    
