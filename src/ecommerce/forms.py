d114dff01ff1e934faac54bffa9c346a3f8551e2
rajhanshdhami <dhamirajhansh.007@gmail.com>
2021-06-30 13:18:10 +0530

    templates and forms fields

---
+++ src/ecommerce/forms.py
@@ -0,0 +1,39 @@
+from django import forms
+
+class ContactForm(forms.Form):
+    full_name = forms.CharField(
+        widget = forms.TextInput(
+            attrs={
+                "class" : "form-control",
+                "id" : "form-full-name",
+                "placeholder" : "Name",
+                "name" : "full_name"
+            }
+        )
+    )
+    email = forms.EmailField(
+        widget = forms.EmailInput(
+            attrs= {
+                "class" : "form-control",
+                "id" : "form_email",
+                "placeholder" : "email",
+                "name" : "email"
+            }
+        )
+    )
+    content = forms.CharField(
+        widget = forms.Textarea(
+            attrs = {
+                "class" : "form-control",
+                "id" : "form_content",
+                "placeholder" : "your content",
+                "name" : "content"
+            }
+        )
+    )
+    #for extra validation we created the clean methods for particular fields
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if not "gmail.com" in email:
+            raise forms.ValidationError("Email has to be gmail.com")
+        return email
---
+++ src/ecommerce/views.py
@@ -0,0 +1,67 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .forms import ContactForm
+
+
+def home_page_old(request):
+    html_="""<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+  <div class = "text-center">
+    <h1>Hello, world!</h1>
+  </div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>"""
+    return HttpResponse(html_)
+
+def home_page(request):
+    context = {
+        "title" : "Hello world",
+        "content" : "Welcome to the home page"
+    }
+    return render(request, "home_page.html", context)   
+
+def about_page(request):
+    context = {
+        "title" : "about page",
+        "content" : "welcome to about page"
+    }    
+    return render(request, "home_page.html", context) 
+
+def contact_page(request):
+    contact_form = ContactForm(request.POST or None)
+    context = {
+        "title" : "contact page",
+        "content" : "welcome to contact page",
+        "form"    : contact_form
+        }    
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
+    #commented because we are passing request.POST in contactform instace
+    """if request.method == "POST":
+        print(request.POST)
+        print(request.POST.get("full_name"))
+        print(request.POST.get("email"))
+        print(request.POST.get("content"))"""
+    return render(request, "contact/view.html", context)      
+
